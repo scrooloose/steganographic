ecf5e56f190cfbbb58955285ede1330e4a1f1597
Martin Grenfell <martin.grenfell@gmail.com>
2012-06-03 00:16:19 +0100

    update the response page
    make the thumbnails bigger and update the layout

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ActiveRecord::Base
   has_attached_file :image,
-                    :styles => { :default => "100x100" },
+                    :styles => { :default => "200x200" },
                     :path => ":rails_root/public/system/:class/:id/:style/:filename",
                     :url => "/system/:class/:id/:style/:filename"
 end
