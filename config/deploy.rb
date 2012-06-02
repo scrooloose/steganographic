c988909bc05602752ebc086fe84dd522ace7ea23
Martin Grenfell <martin.grenfell@gmail.com>
2012-06-02 21:21:00 +0100

    use dev environment when rebuilding the paperclip styles

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ namespace :deploy do
 
   desc "build missing paperclip styles"
   task :build_missing_paperclip_styles, :roles => :app do
-    run "cd #{release_path}; RAILS_ENV=production bundle exec rake paperclip:refresh:missing_styles"
+    run "cd #{release_path}; RAILS_ENV=development bundle exec rake paperclip:refresh:missing_styles"
   end
 end
 
