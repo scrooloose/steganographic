46eaf0db43abd4f802f048390337c12945e1a3fe
Martin Grenfell <martin.grenfell@gmail.com>
2012-06-03 13:58:59 +0100

    fix for emailing from production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,13 @@ Website::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => false
+  }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 end
