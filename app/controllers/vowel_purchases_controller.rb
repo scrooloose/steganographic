537e513d02a902a0d1cdc3a3dc644305e21682b8
Martin Grenfell <martin.grenfell@gmail.com>
2012-06-03 15:23:13 +0100

    make the buy vowels actually use the giverboard API

--- app/controllers/vowel_purchases_controller.rb
+++ app/controllers/vowel_purchases_controller.rb
@@ -5,13 +5,18 @@ class VowelPurchasesController < ApplicationController
   end
 
   def create
-    redirect_to new_challenge_response_path(@resp)
-    @resp.show_vowels = true
-    @resp.save!
+    unless @resp.show_vowels
+      @resp.show_vowels = true
+      @resp.make_giverboard_purchase(current_user)
+      @resp.save!
+    end
+
+    redirect_to new_challenge_response_path(@resp.challenge_id)
   end
 
   protected
     def load_response
       @resp = Response.find(params[:resp_id])
     end
+
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,3 +1,7 @@
+require "net/https"
+require "uri"
+require 'json'
+
 class Response < ActiveRecord::Base
   belongs_to :user
   belongs_to :challenge
@@ -21,5 +25,25 @@ class Response < ActiveRecord::Base
     else
       1
     end
+  end
+
+  def make_giverboard_purchase(user)
+    uri = URI.parse('https://www.giverboard.com/api/v1_1/record_purchase')
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    params = {
+      :api_key => "ba4189f5dffe0e4142ed7e150862403396327b65",
+      :campaign_id => 40,
+      :virtual_object_id => 46,
+      :player_email => user.email
+    }
+
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request.add_field('content-type', 'application/json')
+    request.body = params.to_json
+
+    response = http.request(request)
   end
 end
