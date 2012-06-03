70fdc8a2c21b1c2ef0d12f7bad227a4b6015484d
Martin Grenfell <martin.grenfell@gmail.com>
2012-06-03 16:31:23 +0100

    update images controller - redirect properly after image create

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,16 @@
 class ImagesController < InheritedResources::Base
+  before_filter :authenticate_user!
+
+  def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.new(params[:image])
+    if @image.save
+      redirect_to dashboard_path, :notice => "Successfully created image."
+    else
+      render :action => 'new'
+    end
+  end
 end
