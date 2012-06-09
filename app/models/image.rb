ebd5b4092be66c05b733f69e350743523bae034e
Martin Grenfell <martin.grenfell@gmail.com>
2012-06-09 15:00:20 +0100

    use a nicer html challenge email

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,7 @@ class Image < ActiveRecord::Base
   validates :name, :presence => true
 
   has_attached_file :image,
-                    :styles => { :default => "200x200", :home_page => "150x150", :large => "480x480" },
+                    :styles => { :default => "200x200", :home_page => "150x150", :large => "480x480", :challenge_email => "100x100" },
                     :path => ":rails_root/public/system/:class/:id/:style/:filename",
                     :url => "/system/:class/:id/:style/:filename"
 end
