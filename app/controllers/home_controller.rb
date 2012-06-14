dd07ee9417915099d694223244053e43ee5fd0b4
Martin Grenfell <martin.grenfell@gmail.com>
2012-06-14 23:42:13 +0100

    bug fix for send to friend emails
    when the "friend" clicked the url and signed up it wasnt redirecting
    them to the puzzle as expected

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,9 @@ class HomeController < ApplicationController
   def index
     if user_signed_in?
       if params[:challenge_id]
-        redirect_to new_challenge_response_path(params[:challenge_id])
+        redirect_to_challenge
       else
-        redirect_to_next_challenge :notice => "Hi there, can you solve this??!"
+        redirect_to_challenge
       end
     else
       @user = User.new :email => params[:email]
@@ -29,7 +29,7 @@ class HomeController < ApplicationController
     def login_existing_user
       if @user.valid_password?(params[:user][:password])
         sign_in(@user)
-        redirect_to_next_challenge :notice => "Hi there, can you solve this??!"
+        redirect_to_challenge
       else
         flash.now[:notice] = "An account exists for this email, and has a different password."
         render "index"
@@ -40,10 +40,21 @@ class HomeController < ApplicationController
       @user = User.new(params[:user])
       if @user.save
         sign_in(@user)
-        redirect_to_next_challenge  :notice => "Welcome! Try and solve this!"
+        redirect_to_challenge
       else
         flash.now[:notice] = "There was an error creating your account! #{@user.errors.full_messages.first} "
         render "index"
+      end
+    end
+
+    def redirect_to_challenge
+      challenge_id = params[:challenge_id] || session[:challenge_id]
+      if challenge_id
+        Rails.logger.debug challenge_id.inspect
+        redirect_to new_challenge_response_path(challenge_id.to_s), :notice => "Welcome! Try and solve this!"
+        session.delete(:challenge_id)
+      else
+        redirect_to_next_challenge  :notice => "Welcome! Try and solve this!"
       end
     end
 
