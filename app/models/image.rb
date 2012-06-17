ee9a07430214512ab717637cd53544c903e10c5b
Martin Grenfell <martin.grenfell@gmail.com>
2012-06-17 10:53:03 +0100

    pretty up the challenge email a bit
    its pretty dirty, and could be refactored at some point

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,7 +3,7 @@ class Image < ActiveRecord::Base
   validates_attachment_presence :image
 
   has_attached_file :image,
-                    :styles => { :default => "200x200", :home_page => "150x150", :large => "480x480", :challenge_email => "100x100", :new_challenge_form => "80x80" },
+                    :styles => { :default => "200x200", :home_page => "150x150", :large => "480x480", :new_challenge_form => "80x80" },
                     :path => ":rails_root/public/system/:class/:id/:style/:filename",
                     :url => "/system/:class/:id/:style/:filename"
 end
