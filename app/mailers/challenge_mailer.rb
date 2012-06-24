738551da44b6a53438b5ac30697dc8ac5e2aa076
Martin Grenfell <martin.grenfell@gmail.com>
2012-06-25 00:45:03 +0100

    really change the sender email for challenges

--- app/mailers/challenge_mailer.rb
+++ app/mailers/challenge_mailer.rb
@@ -7,7 +7,6 @@ class ChallengeMailer < ActionMailer::Base
     @challenge = challenge
     @email = email
     mail :to => email || challenge.email,
-         :from => from_email || challenge.user.email,
          :subject => "A Challenge from the Impossible Mission Force"
   end
 end
